f4ad0661052de86f8d44d6e8716936755e941f48
Nouval Rizky <nouvalr@gmail.com>
2025-04-07 00:14:35 +0800

    chore: setup vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,31 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: "src/index.html",
+    },
+    emptyOutDir: true,
+  },
+  optimizeDeps: {
+    // Don't optimize these packages as they contain web workers and WASM files.
+    // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
+    exclude: ["@journeyapps/wa-sqlite", "@powersync/web"],
+    // include: [],
+    include: ["@powersync/web > js-logger"], // <-- Include `js-logger` when it isn't installed and imported.
+    esbuildOptions: {},
+  },
+  plugins: [wasm(), topLevelAwait(), react(), tailwindcss()],
+  worker: {
+    format: "es",
+    plugins: () => [wasm(), topLevelAwait()],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
