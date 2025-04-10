e71fec37f50ca793990270ba5ac0a465193de130
Nouval Rizky <nouvalr@gmail.com>
2025-04-10 12:14:22 +0800

    feat: empty data indicator

--- src/features/dashboard/components/RecentReceiptsCard.tsx
+++ src/features/dashboard/components/RecentReceiptsCard.tsx
@@ -8,6 +8,7 @@ import {
 import RecentReceiptsItem from "./RecentReceiptsItem";
 import { RecentReceipt } from "@/shared/types/receipt";
 import clsx from "clsx";
+import AppEmpty from "@/shared/components/AppEmpty";
 
 function RecentReceiptsCard({
   data,
@@ -22,12 +23,20 @@ function RecentReceiptsCard({
         <CardTitle>Struk Terbaru</CardTitle>
         <CardDescription>Diambil 2 hari terakhir</CardDescription>
       </CardHeader>
-      <CardContent>
-        <div className="flex flex-col gap-6">
-          {data?.map((item) => (
-            <RecentReceiptsItem key={item.id} {...item} />
-          ))}
-        </div>
+      <CardContent className="flex flex-col h-full">
+        {data!.length > 0 && (
+          <div className="flex flex-col gap-6">
+            {data?.map((item) => (
+              <RecentReceiptsItem key={item.id} {...item} />
+            ))}
+          </div>
+        )}
+
+        {data!.length === 0 && (
+          <div className="flex flex-col justify-center items-center flex-1">
+            <AppEmpty />
+          </div>
+        )}
       </CardContent>
     </Card>
   );
